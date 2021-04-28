2c5ab74994b7ee64a4a3fc3f938d61ecaa608358
aakashkumar <aakashthesid@gmail.com>
2021-04-28 22:52:02 +0300

    Prep initial commit

---
+++ src/test/java/com/prep/prepare/PrepareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prep.prepare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrepareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
